a73da8784a159fdbb9a256c8f6d341fe30b0586a
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2022-02-07 00:43:41 +0200

    first

---
+++ flaskapp.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request
+from wtforms import Form, TextAreaField, validators
+from flask_bootstrap import Bootstrap
+import pickle
+import sqlite3
+import os
+import numpy as np
+import joblib
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+
+
+def tokenizer(text):
+    return text.split()
+
+def tokenizer_porter(text):
+    return [porter.stem(word) for word in text.split()]
+
+
+
+
+porter=PorterStemmer()
+loaded_model=joblib.load("./pkl_objects/model.pkl")
+# loaded_stop=joblib.load("./pkl_objects/stopwords.pkl")
+loaded_vec=joblib.load("./pkl_objects/vectorizer.pkl")
+# loaded_vec=TfidfVectorizer(sublinear_tf=True, encoding='utf-8',decode_error='ignore')
+app = Flask(__name__)
+Bootstrap(app)
+
+
+
+
+def classify(document):
+    label = {0: 'negative', 1: 'positive'}
+    X = loaded_vec.transform([document])
+    y = loaded_model.predict(X)[0]
+    
+    proba = np.max(loaded_model.predict_proba(X))
+    return label[y], proba
+
+class ReviewForm(Form):
+    moviereview = TextAreaField('',[validators.DataRequired(),validators.length(min=15)])
+
+@app.route('/')
+def index():
+    form = ReviewForm(request.form)
+    return render_template('reviewform2.html', form=form)
+
+@app.route('/results', methods=['POST'])
+def results():
+    form = ReviewForm(request.form)
+    if request.method == 'POST' and form.validate():
+        review = request.form['moviereview']
+        y, proba = classify(review)
+    return render_template('results.html',content=review,prediction=y,probability=round(proba*100, 2))
+    render_template('reviewform.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
